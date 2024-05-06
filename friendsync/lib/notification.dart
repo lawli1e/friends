4a9d2a270fc3973e498cfe4d9a3ad0e4cdc4b3f8
lawli1e <tristafire99@gmail.com>
2024-05-07 01:14:23 +0700

    Third commit

--- friendsync/lib/notification.dart
+++ friendsync/lib/notification.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NotificationWidget extends StatefulWidget {
   const NotificationWidget({Key? key}) : super(key: key);
@@ -20,9 +21,16 @@ class _NotificationWidgetState extends State<NotificationWidget> {
   }
 
   Future<void> fetchData() async {
-    final response = await http.get(Uri.parse('http://localhost:8080/posts'));
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+
+    final response = await http.get(
+      Uri.parse('http://localhost:8080/notification'),
+      headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer $token'},
+    );
     if (response.statusCode == 200) {
       final List<dynamic> responseData = json.decode(response.body);
+      print(responseData);
       setState(() {
         events = responseData;
       });
@@ -33,38 +41,42 @@ class _NotificationWidgetState extends State<NotificationWidget> {
 
   @override
   Widget build(BuildContext context) {
-    final today = DateTime.now();
-    final filteredEvents = events.where((event) {
-      final eventStart = DateTime.parse(event['eventStart']);
-      final difference = eventStart.difference(today).inDays;
-      return difference == 1; // เหตุการณ์ที่จะถึงใน 1 วัน
-    }).toList();
-
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text(
           'Notification',
-          style: const TextStyle(fontWeight: FontWeight.bold),
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: fetchData,
+          ),
+        ],
       ),
       body: ListView.builder(
-        itemCount: filteredEvents.length,
+        itemCount: events.length,
         itemBuilder: (context, index) {
-          final event = filteredEvents[index];
-          return ListTile(
-            title: Text(event['title'] ?? ''),
-            subtitle: Text('Event Start: ${formatDate(event['eventStart'] ?? '')}'),
-            // สามารถเพิ่มข้อมูลเพิ่มเติมเพื่อแสดงใน ListTile ตามต้องการ
+          final event = events[index];
+          return Card( // ใช้ Card เพื่อให้ Widget ดูสวยงามและมีขอบ
+            margin: EdgeInsets.all(8), // กำหนดระยะห่างรอบขอบของ Card
+            elevation: 4, // เพิ่มเงาให้กับ Card
+            child: ListTile(
+              title: Text(event['title'] ?? ''),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Event Start: ${event['eventStart'] ?? ''}'),
+                  Text('Description: ${event['description'] ?? ''}'),
+                  Text('Max Participants: ${event['maxParticipants'] ?? ''}'),
+                  Text('Category: ${event['category'] ?? ''}'),
+                ],
+              ),
+            ),
           );
         },
       ),
     );
-  }
-
-  String formatDate(String dateTimeString) {
-    DateTime dateTime = DateTime.parse(dateTimeString);
-    String formattedDate = DateFormat.yMd().add_jm().format(dateTime);
-    return formattedDate;
   }
 }
